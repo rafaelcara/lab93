fe0ffb57a13edfb46ff4c0bdf9e0b823229a20c6
Rafael Cara <cara.rafael@gmail.com>
2025-03-20 13:41:08 -0300

    adiciona script

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("%c SYSTEM ONLINE... ", "color: yellow; background: black; font-family: monospace;");
+setTimeout(() => console.log("%c WARNING: Unauthorized access detected! ", "color: red; background: black; font-family: monospace;"), 1000);
+setTimeout(() => console.log("%c Decrypting hidden message... ", "color: cyan; font-family: monospace;"), 2000);
+setTimeout(() => {
+    console.log("%c Success! Secret unlocked: A????", "color: green; font-family: monospace;");
+    console.log("%c01000001 00110101 00110101 01000001 00110001 00110101 01000001 00111001 00111001 01000001 00110111 00110010 01000001 00110000 00111001 01000001 00110110 00110000 00111001 01000001", "color: lime; font-weight: bold;");
+}, 3000);
