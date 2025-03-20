398a4fafb59d9b71dc5f6db091d77273e69a87c7
Rafael Cara <cara.rafael@gmail.com>
2025-03-20 14:01:32 -0300

    ajusta mensagem

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 console.log("%c SYSTEM ONLINE... ", "color: yellow; background: black; font-family: monospace;");
 setTimeout(() => console.log("%c WARNING: Unauthorized access detected! ", "color: red; background: black; font-family: monospace;"), 1000);
 setTimeout(() => console.log("%c Decrypting hidden message... ", "color: cyan; font-family: monospace;"), 2000);
+setTimeout(() => console.log("%c Success! Secret unlocked: A????", "color: green; font-family: monospace;"), 3000);
+setTimeout(() => console.log("%c01000001 00110101 00110101 01000001 00110001 00110101 01000001 00111001 00111001 01000001 00110111 00110010 01000001 00110000 00111001 01000001 00110110 00110000 00111001 01000001", "color: lime; font-weight: bold;"), 3500);
+setTimeout(() => console.log("%c Send: La Donna è Mobile", "color: green; font-family: monospace;"), 4000);
 setTimeout(() => {
-    console.log("%c Success! Secret unlocked: A????", "color: green; font-family: monospace;");
-    console.log("%c01000001 00110101 00110101 01000001 00110001 00110101 01000001 00111001 00111001 01000001 00110111 00110010 01000001 00110000 00111001 01000001 00110110 00110000 00111001 01000001", "color: lime; font-weight: bold;");
     fetch('https://api.ipify.org?format=json')
         .then(response => response.json())
         .then(data => {
@@ -14,5 +15,6 @@ setTimeout(() => {
         });
 
         console.log("%c Sending IP to N.O.B.O.D.Y. ... ", "color: yellow; background: black; font-family: monospace;");
+        
+}, 5000);
 
-}, 3000);
